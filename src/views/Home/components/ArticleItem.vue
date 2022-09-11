b2f701ca431844f6239c4535e6419a77f13ea2e3
钗头凤 <2806398143@qq.com>
2022-09-11 16:46:10 +0800

    feat:首页结构完成

---
+++ src/views/Home/components/ArticleItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <!-- 没有 -->
+    <van-cell
+      v-if="article.cover.type === 0"
+      :title="article.title"
+      :label="label"
+    />
+    <!-- 一张 -->
+    <van-cell
+      v-else-if="article.cover.type === 1"
+      :title="article.title"
+      :label="label"
+    />
+    <!-- 三张 -->
+    <van-cell v-else title="三张">
+      <template #label>
+        <van-image
+          v-for="(item, index) in article.cover.images"
+          :key="index"
+          width="100"
+          height="100"
+          :src="item"
+        />
+      </template>
+    </van-cell>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  computed: {
+    label() {
+      const { aut_name, comm_count, pubdata } = this.article
+      return `${aut_name} ${comm_count}评论 ${pubdata}`
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ src/views/Home/components/ArticleList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <article-item
+      v-for="item in articles"
+      :key="item.art_id"
+      :article="item"
+    ></article-item>
+  </div>
+</template>
+
+<script>
+import { getArticle } from '@/api'
+
+import ArticleItem from './ArticleItem.vue'
+export default {
+  name: 'Article',
+  components: { ArticleItem },
+  props: {
+    id: [String, Number]
+  },
+  data() {
+    return {
+      articles: []
+    }
+  },
+  created() {
+    this.getFirstPageArticle()
+  },
+  methods: {
+    async getFirstPageArticle() {
+      try {
+        const { data } = await getArticle(this.id, +new Date())
+        this.articles = data.data.results
+      } catch (error) {
+        const status = error.response?.status
+        if (!error.response || status === 507) {
+          throw error
+        } else {
+          if (status === 400) {
+            throw new Error(error.response.data.message)
+          }
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style></style>
